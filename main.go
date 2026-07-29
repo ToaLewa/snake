a83d2aae128e9b305a997083eaa75e9f1e47404e
Toa Lewa <kooltone16@gmail.com>
2026-07-28 22:55:06 -0500

    Start basic animation

--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
-	rl.InitWindow(800, 450, "Snake")
+	rl.InitWindow(800, 400, "Snake")
 	defer rl.CloseWindow()
 
-	rl.SetTargetFPS(1)
+	var y int32 = 0
+
+	rl.SetTargetFPS(10)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawRectangle(0, 0, 20, 20, rl.Black)
+		rl.DrawRectangle(0, y, 20, 20, rl.Black)
 		// rl.DrawRectangle(0, 20, 20, 20, rl.Black)
 		rl.EndDrawing()
+
+		y += 20
 	}
 }
