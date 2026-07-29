1a6ffa521529084a183df44b70b807eeaf5ee207
Toa Lewa <kooltone16@gmail.com>
2026-07-28 23:12:07 -0500

    Refactor

--- main.go
+++ main.go
@@ -1,22 +1,49 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	// "fmt"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type winStats struct {
+	width  int32
+	height int32
+}
+
+var windowDetails = winStats{width: 800, height: 400}
+
+const gridIncrement = 20
+
+func onBorder(x int, y int) bool {
+	return true
+}
 
 func main() {
-	rl.InitWindow(800, 400, "Snake")
+	rl.InitWindow(windowDetails.width, windowDetails.height, "Snake")
 	defer rl.CloseWindow()
 
 	var y int32 = 0
+	var x int32 = 0
+	// direction := "s"
 
 	rl.SetTargetFPS(10)
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawRectangle(0, y, 20, 20, rl.Black)
-		// rl.DrawRectangle(0, 20, 20, 20, rl.Black)
+		rl.DrawRectangle(x, y, gridIncrement, gridIncrement, rl.Black)
 		rl.EndDrawing()
 
-		y += 20
+		bottom := y == windowDetails.height-gridIncrement
+
+		if !bottom {
+			y += gridIncrement
+		} else {
+			x += gridIncrement
+		}
+
+		// fmt.Printf("%d\n", y)
+
 	}
 }
