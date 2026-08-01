42ab673a35eae52d8f5db664f88bc94c11b9bf01
Toa Lewa <kooltone16@gmail.com>
2026-07-31 20:52:58 -0500

    Add WASD keymaps

--- input.go
+++ input.go
@@ -1,17 +1,15 @@
-//go:build raylib
-
 package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 func getInputDirection() (Direction, bool) {
-	if rl.IsKeyPressed(rl.KeyUp) {
+	if rl.IsKeyPressed(rl.KeyUp) || rl.IsKeyPressed(rl.KeyW) {
 		return North, true
-	} else if rl.IsKeyPressed(rl.KeyDown) {
+	} else if rl.IsKeyPressed(rl.KeyDown) || rl.IsKeyPressed(rl.KeyS) {
 		return South, true
-	} else if rl.IsKeyPressed(rl.KeyLeft) {
+	} else if rl.IsKeyPressed(rl.KeyLeft) || rl.IsKeyPressed(rl.KeyA) {
 		return West, true
-	} else if rl.IsKeyPressed(rl.KeyRight) {
+	} else if rl.IsKeyPressed(rl.KeyRight) || rl.IsKeyPressed(rl.KeyD) {
 		return East, true
 	} else {
 		return "", false
